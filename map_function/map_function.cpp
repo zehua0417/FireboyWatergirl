fc2e72050d389b297c7575e1c4abab22f901d061
zehua0417 <zehuali0417@gmail.com>
2022-12-25 19:42:54 +0800

    try

---
+++ map_function/map_function.cpp
@@ -0,0 +1,173 @@
+//#undef UNICODE
+//#undef _UNICODE
+#include<stdio.h>
+#include<easyx.h>
+#include"map_function.h"
+#define WIDTH 640
+#define HEIGHT 480
+#define NUM_evt 10
+
+IMAGE map;
+
+/**
+ * @brief 图宝石链表节点(各难度设置一个文件)
+ */
+struct diamond
+{
+    int type;   //1->蓝宝石, 2->红宝石, 3->绿宝石
+    float x;   //宝石的横坐标
+    float y;   //宝石的纵坐标
+    int n;  //宝石编号,便于修改删除
+    struct diamond *next;
+};
+
+struct diamond *head = NULL;
+struct diamond *end = NULL;
+
+/**
+ * @brief 地板或天花板模型结构体(来自CSDN)
+ */
+typedef struct model1
+{
+	int x1 = 0, x2 = 1100, y1 = 699, y2 = 714;//714
+}model1_;
+
+model1_ floor[10][100], roof[10][100];//模型结构体数组(每个地图一个)
+
+/**
+ * @brief 左右墙壁模型结构体(来自CSDN)
+ */
+typedef struct model2
+{
+	int x = 369, y1 = 617, y2 = 621;
+}model2_;
+
+model2_ wallleft[10][100], wallright[10][100];//模型结构体数组(每个地图一个)
+
+/**
+ * @brief 电梯结构体
+*/
+struct evt
+{
+    int x;  //横坐标
+    int y;  //纵坐标
+    int n;  //电梯编号,便于和按钮对应
+    int type;   //运动类型: 1,上->下; 2,下->上; 3,左->右; 4,右->左
+    double distance;    //可以滑行的距离
+    double dis_now;     //电梯此时的位置(相对起点的距离)<=distance
+};
+
+struct evt lowlevel_wvt[NUM_evt];    //设置电梯结构体数组, 每个关卡一个
+
+/**
+ * @brief 按钮结构体
+*/
+struct button
+{
+    int x;  //横坐标
+    int y;  //纵坐标
+    int n;  //编号
+    double status;  //状态即被按下的深度
+};
+
+struct button lowlevel_button[NUM_evt];  //设置按钮结构体数组, 每个关卡一个
+
+//=====================================================================================
+//=====================================================================================
+//=====================================================================================
+
+void test()
+{
+    initgraph(WIDTH,HEIGHT);
+    BeginBatchDraw();
+
+    set_map(1);
+
+    FlushBatchDraw();
+}
+
+int main (void)
+{
+    test();
+    return 0;
+}
+
+void set_map(int dif)
+{
+    switch(dif)
+    {
+        case 1: 
+            loadimage(&map,_T("./image/lowlevel_map"),WIDTH,HEIGHT);
+            putimage(0,0,&map);
+            break;
+        case 2:
+            loadimage(&map,_T("./image/hightlevel_map"),WIDTH,HEIGHT);
+            putimage(0,0,&map);
+            break;
+    }
+}
+
+//================================================================================
+
+void init_diamondlist()
+{
+    head = (struct diamond *)malloc(sizeof(struct diamond));
+    head->next = NULL;
+    end = head;
+}
+
+void *load_diamond(int  dif)
+{
+    init_diamondlist();
+    struct diamond *ptr = (struct diamond *)malloc(sizeof(struct diamond));
+    ptr->next = NULL;
+
+    FILE *fp = NULL;
+    switch(dif)
+    {
+        case 1:
+            fp = fopen("./data/lowlevel_diamond.txt","a+");
+            break;
+        case 2:
+            fp = fopen("./data/hightlevel_diamond.txt","a+");
+    }
+    while(fscanf(fp,"%d %f %f %d\n"))
+    {
+        if(head->next = NULL)
+        {
+            head->next = ptr;
+        }
+        else
+        {
+            end->next = ptr;
+            end = end->next;
+        }
+        ptr = (struct diamond*)malloc(sizeof(struct diamond));
+        ptr ->next = NULL;
+    }
+    free(ptr);
+    fclose(fp);
+}
+
+void del_diamond(int n)
+{
+    struct diamond *pre = head;
+    while(20030417)
+    {
+        if (pre->next&&(pre->next->n == n))
+        {
+            struct diamond *q = pre->next;
+            pre->next = pre->next->next;
+            free(q);
+            return;
+        }
+        else
+        {
+            if(pre->next != NULL)
+                pre = pre->next;
+            else
+                return;
+        }
+    }
+}
+
---
+++ map_function/map_function.h
@@ -0,0 +1,60 @@
+#ifndef define _MAP_FUNC
+
+/**
+ * @brief 根据难度选择, 打印地图
+ * @param dif 难度,见select_dif()函数
+ */
+void set_map(int dif);
+
+void init_diamondlist();
+
+/**
+ * @brief 从文件读入宝石信息,生成链表
+ * @param dif nandu,见select_dif()函数
+ */
+void *load_diamond(int  dif);
+
+/**
+ * @brief 从链表中删除n号宝石
+ * @param dif 难度
+ * @param n 要删除的宝石编号
+ * 
+ * @return struct diamond *head 宝石链表表头
+ */
+struct diamond *del_diamond(int dif, int n);
+
+/**
+ * @brief 打印宝石
+ * @param head 宝石链表表头
+ */
+void set_diamond(struct diamond *head);
+
+/**
+ * @breif 从文件读入电梯信息
+ * @param dif 难度
+ * 
+ * @return 电梯结构体数组首地址
+ */
+struct evt *laod_evt(int dif);
+
+/**
+ * @brief 打印电梯
+ * @param evt_head 电梯结构体数组首地址
+ */
+void set_evt(struct evt *evt_head);
+
+/**
+ * @brief 从文件读入按钮信息
+ * @param dif 难度
+ * 
+ * @return 按钮结构体数组收哦地址
+ */
+struct button *load_button(int dif);
+
+/**
+ * @brief 打印按钮
+ * @param button_head 按钮结构体数组首地址
+ */
+void set_button(struct button *button_head);
+
+#endif
